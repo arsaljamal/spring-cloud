b33c85453ec4ca69e18abcce6fa366dc93ced96b
arsaljamal <arsaljamal2348@gmail.com>
2021-12-13 17:43:45 +0500

    setting up config-server

---
+++ config-server/src/test/java/com/arsal/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsal.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
