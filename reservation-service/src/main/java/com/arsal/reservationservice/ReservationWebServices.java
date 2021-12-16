80c77fd5bde1ccddbbac64cde4abaa77b7aa710d
arsaljamal <arsaljamal2348@gmail.com>
2021-12-16 15:18:05 +0500

    adding spring cloud load balancer

--- reservation-service/src/main/java/com/arsal/reservationservice/ReservationWebServices.java
+++ reservation-service/src/main/java/com/arsal/reservationservice/ReservationWebServices.java
@@ -1,10 +1,11 @@
 package com.arsal.reservationservice;
 
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -18,16 +19,23 @@ public class ReservationWebServices {
 
     private final ReservationRepository reservationRepository;
     private final RestTemplate restTemplate;
+    private LoadBalancerClient loadBalancer;
 
-    public ReservationWebServices(ReservationRepository reservationRepository, RestTemplate restTemplate) {
+    public ReservationWebServices(ReservationRepository reservationRepository, RestTemplate restTemplate, LoadBalancerClient loadBalancerClient) {
         this.reservationRepository = reservationRepository;
         this.restTemplate = restTemplate;
+        this.loadBalancer = loadBalancerClient;
     }
 
     @GetMapping(path = "/customers")
     public List<Customer> getAllCustomers() {
+
+        ServiceInstance serviceInstance=loadBalancer.choose("customerservices");
+        String baseUrl=serviceInstance.getUri().toString();
+        baseUrl=baseUrl+"/customers";
+
         ResponseEntity<List<Customer>> customerResponse = this.restTemplate.exchange
-                ("http://CUSTOMERSERVICES/customers", HttpMethod.GET, null,
+                (baseUrl, HttpMethod.GET, null,
                         new ParameterizedTypeReference<>() {
         });
         return customerResponse.getBody();
@@ -35,8 +43,12 @@ public class ReservationWebServices {
 
     @GetMapping(path = "/vehicles")
     public List<Vehicle> getAllVehicles() {
+        ServiceInstance serviceInstance=loadBalancer.choose("vehicleservices");
+        String baseUrl=serviceInstance.getUri().toString();
+        baseUrl=baseUrl+"/vehicles";
+
         ResponseEntity<List<Vehicle>> vehicleResponse = this.restTemplate.exchange
-                ("http://VEHICLESERVICES/vehicles", HttpMethod.GET, null,
+                (baseUrl, HttpMethod.GET, null,
                         new ParameterizedTypeReference<>() {
                         });
         return vehicleResponse.getBody();
